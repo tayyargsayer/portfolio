506b55631fa2b05417e1f58d7ab9e8f0ce5deffb
tayyargsayer <tgorkemsayer@gmail.com>
2024-12-01 14:12:01 +0300

    1.12.2024 update

--- app.py
+++ app.py
@@ -9,6 +9,20 @@ def hello_world():  # put application's code here
     return render_template('index.html')
 
 
+@app.route('/portfolio-details')
+def details():
+    return render_template('portfolio-details.html')
+
+@app.route('/service-details')
+def service_details():
+    return render_template('service-details.html')
+
+@app.route('/starter-page')
+def starter_page():
+    return render_template('starter-page.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
